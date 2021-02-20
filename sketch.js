40a6ffe569f636d8198299e149aa653447e14503
Akshat-Dubey <77731099+Akshat-Dubey@users.noreply.github.com>
2021-02-20 16:14:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var a,b;
+
+
+function setup() {
+  createCanvas(800,400);
+  a=createSprite(400, 200, 50, 50);
+  b=createSprite(200,200,100,100);
+  a.shapeColor="lightblue";
+  b.shapeColor="lightblue";
+}
+
+function draw() {
+  background(255,255,255); 
+  console.log(a.x-b.x);
+  a.x=World.mouseX;
+  a.y=World.mouseY;
+
+if(a.x-b.x<=a.width/2+b.width/2 && b.x-a.x<=a.width/2+b.width/2 && a.y-b.y<=a.height/2+b.height/2 && b.y-a.y<=a.height/2+b.height/2){
+  a.shapeColor="lightpink";
+  b.shapeColor="lightpink";
+}
+else{
+  a.shapeColor="lightblue";
+  b.shapeColor="lightblue";
+
+}
+
+
+
+  drawSprites();
+}
